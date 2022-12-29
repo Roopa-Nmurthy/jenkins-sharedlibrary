c1290f55d78572b06e2d1ccd5e2b8b91584d678c
Roopa N <116476267+Roopa-Nmurthy@users.noreply.github.com>
2022-12-29 11:39:11 +0530

    Update eksShell.groovy

--- vars/eksShell.groovy
+++ vars/eksShell.groovy
@@ -37,7 +37,7 @@ environment {
         }
         
         stage('DEPLOY IMAGE') {
-		agent{label 'eks'}
+		agent{label 'Master'}
 		          steps {
 			            sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
 		          }
